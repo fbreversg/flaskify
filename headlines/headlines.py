acd2607d110240076576c4050f1c1ba3304de4c6
Francisco Brevers <franciscobrevers@MacBook-Pro-de-Francisco.local>
2016-08-25 14:53:56 +0200

    Integracion con OpenWeather (JSON)

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,7 +1,13 @@
 import feedparser
 from flask import Flask, render_template, request
+import json
+import urllib
 
 app = Flask(__name__)
+
+DEFAULTS = {'publication': 'bbc',
+            'city': 'Madrid, ES'}
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=cf02ecd530b6def7dabb9150383bdb72"
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -10,14 +16,38 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
+def get_home():
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("home.html", articles=articles, weather=weather)
+
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
+        publication = DEFAULTS["publication"]
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed['entries'])
+    return feed['entries']
+
+
+def get_weather(query):
+    query = urllib.parse.quote(query)
+    url = WEATHER_URL.format(query)
+    data = urllib.request.urlopen(url).read().decode('utf8')  # ojo con este decode
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":parsed["weather"][0]["description"], "temperature":parsed["main"]["temp"], "city":parsed["name"]
+                  }
+    return weather
 
 if __name__ == '__main__':
     app.run(port=7000, debug=True)
