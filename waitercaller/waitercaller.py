965aa0f7b27cacd3d26e20bbbfbd0f6bce7b8dbc
Francisco Brevers <franciscobrevers@MacBook-Pro-de-Francisco.local>
2016-09-01 10:24:42 +0200

    Añadida sal y tema passwords

--- waitercaller/waitercaller.py
+++ waitercaller/waitercaller.py
@@ -3,14 +3,17 @@ from flask import render_template
 from flask_login import LoginManager
 from flask_login import login_required
 from flask_login import login_user
+from flask_login import logout_user
 from flask_login import redirect
 from flask_login import url_for
 from flask_login import request
 
 from mockdbhelper import MockDBHelper
 from user import User
+from passwordhelper import PasswordHelper
 
 DB = MockDBHelper()
+PH = PasswordHelper()
 
 app = Flask(__name__)
 app.secret_key = 'tPXJY3X37Qybz4QykV+hOyUxV2342fdsfasQeEXf1Ao2C8upz4addf34s4dasdfasdasbdvsfvass+s+fGQXKsM'
@@ -26,10 +29,10 @@ def home():
 def login():
     email = request.form.get("email")
     password = request.form.get("password")
-    user_password = DB.get_user(email)
-    if user_password and user_password == password:
+    stored_user = DB.get_user(email)
+    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
         user = User(email)
-        login_user(user)
+        login_user(user, remember=True)         # persistencia de login
         return redirect(url_for('account'))
     return home()
 
@@ -45,6 +48,27 @@ def load_user(user_id):
 @login_required
 def account():
     return "You are logged in."
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form.get("email")
+    pw1 = request.form.get("password")
+    pw2 = request.form.get("password2")
+    if not pw1 == pw2:
+        return redirect(url_for('home'))
+    if DB.get_user(email):
+        return redirect(url_for('home'))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(port=7000, debug=True)
